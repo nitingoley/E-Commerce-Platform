eb8aea78fb6f3b3353f4a7880f9ce96aae1d42ad
Nitin Goley <113915570+nitingoley@users.noreply.github.com>
2025-09-05 10:26:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -33,14 +33,11 @@ app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
-// ✅ Serve frontend build in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
